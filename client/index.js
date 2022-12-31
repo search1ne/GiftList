d1ca141de77a761161e1dfdece6e1dfc02288dc0
search1ne <114374119+search1ne@users.noreply.github.com>
2022-12-30 21:19:56 -0800

    latest build

--- client/index.js
+++ client/index.js
@@ -1,14 +1,31 @@
 const axios = require('axios');
 const niceList = require('../utils/niceList.json');
 const MerkleTree = require('../utils/MerkleTree');
+const verifyProof = require('../utils/verifyProof');
 
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  // TODO: how do we prove to the server we're on the niceList? 
+  // 1. create a merkle tree for the whole nice list
+  const merkleTree = new MerkleTree(niceList);
+  const root = merkleTree.getRoot();
+  console.log({ root })
+  
+  // 2. find the proof that your name is in the list
+  const name = 'Ruben Toy';
+  const index = niceList.findIndex(n => n === name);
+  const proof = merkleTree.getProof(index);
 
-  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+  // verify proof against the Merkle Root
+  console.log({proof, name, root})
+  console.log( verifyProof(proof, name, root) );
+
+  // 3. send the proof to the server
+  const { data: gift } = await axios.post(`${serverUrl}/gift`, {    
+    proof,
+    name,
+    root
   });
 
   console.log({ gift });
--- utils/verifyProof.js
+++ utils/verifyProof.js
@@ -3,11 +3,11 @@ const { hexToBytes, bytesToHex } = require('ethereum-cryptography/utils');
 
 const concat = (left, right) => keccak256(Buffer.concat([left, right]));
 
-function verifyProof(proof, leaf, root) {
+function verifyProof(proof, name, root) {
   proof = proof.map(({data, left}) => ({ 
     left, data: hexToBytes(data)
   }));
-  let data = keccak256(Buffer.from(leaf));
+  let data = keccak256(Buffer.from(name));
 
   for (let i = 0; i < proof.length; i++) {
     if (proof[i].left) {
